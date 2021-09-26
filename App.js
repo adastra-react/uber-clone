d753d165636eee078bc94ea79d9b91320af3a9f8
Suneil England <70965512+adastra-react@users.noreply.github.com>
2021-09-25 21:32:39 -0500

    uber clone initial commit

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider>
+      <View style={styles.container}>
+        <Text>Lets gooooooo!</Text>
+      </View>
+    </Provider>
   );
 }
 
