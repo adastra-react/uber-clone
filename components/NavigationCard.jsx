11e6651051d88106bf6a3bc84512f83f6884022e
Suneil England <70965512+adastra-react@users.noreply.github.com>
2021-09-29 02:24:11 -0500

    added the stack navigator for the ride opstions card and navigation card

---
+++ components/NavigationCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const NavigationCard = () => {
+    return (
+        <View>
+            <Text>sup</Text>
+        </View>
+    )
+}
+
+export default NavigationCard
+
+const styles = StyleSheet.create({})
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,15 +3,35 @@ import { StyleSheet, Text, View } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import Map from '../components/Map';
 import MapView from 'react-native-maps';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import NavigationCard from '../components/NavigationCard';
+import RideOptionsCard from '../components/NavigationCard';
 
 const MapScreen = () => {
+    const Stack = createNativeStackNavigator();
+
     return (
         <View>
             <View style={tw`h-1/2`}>
                 <Map/>
             </View>
             <View style={tw`h-1/2`}>
-
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="NavigationCard"
+                        component={NavigationCard}
+                        options={{
+                            headerShown: false
+                        }}
+                    />
+                    <Stack.Screen
+                        name="RideOptionsCard"
+                        component={RideOptionsCard}
+                        options={{
+                            headerShown: false
+                        }}
+                    />
+                </Stack.Navigator>
             </View>
         </View>
     )
