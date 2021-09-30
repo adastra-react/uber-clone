20aa1b496c8bb6fb48f74248578a9a96ba17172a
Suneil England <70965512+adastra-react@users.noreply.github.com>
2021-09-29 21:13:06 -0500

    item separator added

--- components/NavFavorites.jsx
+++ components/NavFavorites.jsx
@@ -24,8 +24,13 @@ const NavFavorites = () => {
             <FlatList 
                 data={data}
                 keyExtractor={(item) => item.id}
+                ItemSeparatorComponent={() => (
+                    <View
+                        style={[tw`bg-gray-200`, { height: 0.5 }]}
+                    />
+                )}
                 renderItem={({ item: {location, destination, icon} }) => (
-                    <TouchableOpacity>
+                    <TouchableOpacity style={tw`flex-row items-center p-5`} >
                         <Icon
                             style={tw`mr-4 rounded-full bg-gray-300 p-3`}
                             name={icon}
@@ -33,6 +38,10 @@ const NavFavorites = () => {
                             color="white"
                             size={18}
                         />
+                        <View>
+                            <Text style={tw`font-semibold text-lg`} >{location}</Text>
+                            <Text style={tw`text-gray-500`} >{destination}</Text>
+                        </View>
                     </TouchableOpacity>
                 )}
              />
