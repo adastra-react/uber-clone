9a5361c7759f85ca40d4135b3e3549fa17d9d95e
Suneil England <70965512+adastra-react@users.noreply.github.com>
2021-09-30 00:46:09 -0500

    semi final uber drive build

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import Map from '../components/Map';
 import MapView from 'react-native-maps';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import NavigationCard from '../components/NavigationCard';
 import RideOptionsCard from '../components/RideOptionsCard';
+import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/core';
 
 const MapScreen = () => {
     const Stack = createNativeStackNavigator();
+    const navigation = useNavigation();
 
     return (
         <View>
+            <TouchableOpacity
+            onPress={() => {
+                navigation.navigate("HomeScreen")
+            }}
+             style={tw`bg-gray-100 p-1 absolute top-16 left-8 z-50 rounded-full shadow-lg`}>
+                <Icon
+                    name="home"
+                />
+            </TouchableOpacity>
             <View style={tw`h-1/2`}>
                 <Map/>
             </View>
