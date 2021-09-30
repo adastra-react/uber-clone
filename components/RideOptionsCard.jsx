ee0e32ea78f2f67e69adbfbd1fb9da25c1ef8a3a
Suneil England <70965512+adastra-react@users.noreply.github.com>
2021-09-29 21:54:15 -0500

    start to work on the RideOptionsCardComponent

--- components/RideOptionsCard.jsx
+++ components/RideOptionsCard.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Icon } from 'react-native-elements/dist/icons/Icon';
 import tw from 'tailwind-react-native-classnames';
 
 const RideOptionsCard = () => {
 
 
     return (
-        <SafeAreaView>
+        <SafeAreaView style={tw`bg-white flex-1`}>
+            <View>
+                <TouchableOpacity>
+                    <Icon name="chevron-left" type="fontawesom" />
+                </TouchableOpacity>
+            </View>
             <Text style={tw`text-center py-5 text-xl`} >Select a Ride</Text>
         </SafeAreaView>
     )
