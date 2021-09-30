fd98aeee809ca449ea41d14c534e2cb2f7f9aace
Suneil England <70965512+adastra-react@users.noreply.github.com>
2021-09-29 23:09:02 -0500

    implemented the back navigation button in the ride option component

--- components/RideOptionsCard.jsx
+++ components/RideOptionsCard.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Icon } from 'react-native-elements/dist/icons/Icon';
 import tw from 'tailwind-react-native-classnames';
+import { useNavigation } from '@react-navigation/core';
 
 const RideOptionsCard = () => {
 
+    const navigation = useNavigation();
 
     return (
         <SafeAreaView style={tw`bg-white flex-1`}>
-            <View>
-                <TouchableOpacity>
-                    <Icon name="chevron-left" type="fontawesom" />
+            <View >
+                <TouchableOpacity onPress={() => navigation.navigate('NavigationCard')}
+                    style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}
+                >
+                    <Icon name="chevron-left" type="fontawesom"/>
                 </TouchableOpacity>
+                <Text style={tw`text-center py-5 text-xl`} >Select a Ride</Text>
             </View>
-            <Text style={tw`text-center py-5 text-xl`} >Select a Ride</Text>
+           <FlatList/>
         </SafeAreaView>
     )
 }
