a39efe3e9c1d32f0c7c2d5e44d965bc4bfc1c1a2
Suneil England <70965512+adastra-react@users.noreply.github.com>
2021-09-29 23:34:02 -0500

    list of uber cars added to the UI

--- components/RideOptionsCard.jsx
+++ components/RideOptionsCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { FlatList, Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Icon } from 'react-native-elements/dist/icons/Icon';
 import tw from 'tailwind-react-native-classnames';
 import { useNavigation } from '@react-navigation/core';
@@ -11,20 +11,20 @@ const RideOptionsCard = () => {
     const data = [
         {
             id: "Uber-X-123",
-            title: "UberX",
+            title: "Uber X",
             multiplier: 1,
             image: "https://links.papareact.com/3pn"
         },
         {
             id: "Uber-X-456",
             title: "Uber XL",
-            multiplier: 1,
+            multiplier: 1.2,
             image: "https://links.papareact.com/5w8"
         },
         {
             id: "Uber-X-789",
             title: "Uber LUX",
-            multiplier: 1,
+            multiplier: 1.7,
             image: "https://links.papareact.com/7pf"
         },
     ]
@@ -39,7 +39,26 @@ const RideOptionsCard = () => {
                 </TouchableOpacity>
                 <Text style={tw`text-center py-5 text-xl`} >Select a Ride</Text>
             </View>
-           <FlatList/>
+           <FlatList data={data}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item: { id, title, multiplier, image } }) => (
+                    <TouchableOpacity style={tw`flex-row items-center justify-between px-10`} >
+                        <Image
+                            style={{
+                                width: 100,
+                                height: 100,
+                                resizeMode: "contain"
+                            }}
+                            source={{ uri: image }}
+                        />
+                        <View style={tw`-ml-6`} >
+                            <Text style={tw`text-xl font-semibold`} >{title}</Text>
+                            <Text>travel time</Text>
+                        </View>
+                        <Text style={tw`text-xl`} >$300</Text>
+                    </TouchableOpacity>
+                )}
+            />
         </SafeAreaView>
     )
 }
